03f774a5b64f1c3824a044f58a3bc250ee8acc3a
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2016-04-09 18:44:13 +0200

    new sha1 hashes

--- sha1-hashes.php
+++ sha1-hashes.php
@@ -2,9 +2,18 @@
 
 $data = array(
     "linux \n a" => "linux \\n a",
+    "linux \r\n a" => "linux \\r\\n a",
     "win \r\n a" => "win \\r\\n a",
+    "win \n a" => "win \\n a",
     "mix \r\n a \n b" => "mix \\r\\n a \\n b",
+    "mix \n a \n b" => "mix \\n a \\n b",
+    "mix \r\n a \r\n b" => "mix \\r\\n a \\r\\n b",
     "all \r\n a \n b \r c" => "all \\r\\n a \\n b \\r c",
+    "all \n a \n b \r c" => "all \\n a \\n b \\r c",
+    "all \r\n a \r\n b \r c" => "all \\r\\n a \\r\\n b \\r c",
+    "\0\1\2\0 \n \r\n \r \0\1\2\0" => "\\0\\1\\2\\0 \\n \\r\\n \\r \\0\\1\\2\\0",
+    "\0\1\2\0 \n \n \r \0\1\2\0" => "\\0\\1\\2\\0 \\n \\n \\r \\0\\1\\2\\0",
+    "\0\1\2\0 \r\n \r\n \r \0\1\2\0" => "\\0\\1\\2\\0 \\r\\n \\r\\n \\r \\0\\1\\2\\0",
 );
 
 echo "=============================================\n";
